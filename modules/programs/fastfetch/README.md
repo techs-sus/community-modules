7771c53597e48e2129b450135e8d31df34bad31a
Conor <conorboyle07@protonmail.com>
2026-06-03 22:46:59 +0100

    programs/fastfetch: init

---
+++ modules/programs/fastfetch/README.md
@@ -0,0 +1,5 @@
+# fastfetch
+
+Since our attempt to add finix to fastfetch was [rejected upstream](https://github.com/fastfetch-cli/fastfetch/pull/2344), I made this module to make it easy to get the finix logo into fastfetch!
+
+Very crucial stuff.
---
+++ modules/programs/fastfetch/default.nix
@@ -0,0 +1,37 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  cfg = config.programs.fastfetch;
+in
+{
+  options.programs.fastfetch = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = ''
+        Whether to enable [fastfetch](${pkgs.fastfetch.meta.homepage}).
+      '';
+    };
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.fastfetch.overrideAttrs (old: {
+        patches = (old.patches or []) ++ [
+          ./fastfetch.patch
+        ];
+      });
+      defaultText = lib.literalExpression "pkgs.fastfetch";
+      description = ''
+        The package to use for `fastfetch`.
+      '';
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = [ (lib.hiPrio cfg.package) ];
+  };
+}
